4b5a8ed979c727fe5471c007f071d2850580cea9
dev <jaysebagh@gmail.com>
2024-07-19 13:50:09 -0700

    added name, pfp, and started navigation

--- src/Landing.js
+++ src/Landing.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './assets/styles/landing.css';
+import pfp from './assets/images/pfp.jpg';
 
 const Landing = () => {
   return (
     <div className="main-container">
       <div className="panel-glow">
         <div className="left-panel">
-
+          <h1>Jean-Francois Sebagh</h1>
+          <img className="pfp" src={pfp}/>
+          <h2>Navigation</h2>
+          <div className='nav'>
+            <div>Projects</div>
+            <div>Skills</div>
+            <div>Contact</div>
+          </div>
         </div>
         <div className='main-panel'>
           hello
