f29a2e4860c5cc49c241a5785f1d249078783483
dev <jaysebagh@gmail.com>
2024-07-19 13:14:50 -0700

    added background and created main panel

---
+++ src/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './assets/styles/landing.css';
+
+const Landing = () => {
+  return (
+    <div className="main-container">
+      <div className="panel-glow">
+        <div className="left-panel">
+
+        </div>
+        <div className='main-panel'>
+          hello
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './assets/styles/index.css';
+import Landing from './Landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Landing />
   </React.StrictMode>
 );
