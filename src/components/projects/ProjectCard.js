4f45f485555d54c7d492ffa5c8c847a3485c18ac
dev <jaysebagh@gmail.com>
2024-07-25 11:51:33 -0700

    completed projects functionality and styling

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,21 +1,25 @@
 import React from "react";
-// import '../styles/card.css';
+import '../../assets/styles/projectcard.css'
 
 const ProjectCard = ({ data } ) => {
     return(
-        <div>
-            <img src={data.url} alt="project preview"/>
-            <h1>{data.title}</h1>
-            <p>{data.bio}</p>
-            <h2>{data.stack}</h2>
-            <div>
-                <a href={`//${data.github}`} target="_blank" rel="noreferrer">
-                    <button>REPO</button>
-                </a>
-                <a href={`//${data.demo}`} target="_blank" rel="noreferrer">
-                    <button>DEMO</button>
-                </a>
+        <div className="project-card-container">
+            <div className="upper-card">
+                <div className="left-card">
+                    <h1>{data.title}</h1>
+                    <h2>{data.stack}</h2>
+                    <div>
+                        <a className="repo-button" href={`//${data.github}`} target="_blank" rel="noreferrer">
+                            REPO
+                        </a>
+                        <a className="demo-button" href={`//${data.demo}`} target="_blank" rel="noreferrer">
+                            DEMO
+                        </a>
+                    </div>
+                </div>
+                <img src={data.url} alt="project preview"/>
             </div>
+            <p>{data.bio}</p>
         </div>
     )
 };
--- src/components/tabs/Projects.js
+++ src/components/tabs/Projects.js
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import projectsDATA from '../../assets/data/projectsData.json';
 import ProjectCard from '../projects/ProjectCard';
+import '../../assets/styles/projects.css';
 
 const Projects = () => {
     const [search, setSearch] = useState("");
 
     return (
-        <div>
-        <input
-            type='text'
-            placeholder='search...'
-            value={ search }
-            onChange={(e) => setSearch(e.target.value)}
-        />
-
-        <div>
-            {projectsDATA.filter((item) =>
-                item.title.toLowerCase().includes(search.toLowerCase())
-            ).map((data, index) => (
-                <ProjectCard key={index} data={data}/>
-            ))}
-        </div>
+        <div className="projects-tab-container">
+            <input
+                className="projects-search-bar"
+                type='text'
+                placeholder='search...'
+                value={ search }
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <div>
+                {projectsDATA.filter((item) =>
+                    item.title.toLowerCase().includes(search.toLowerCase())
+                ).map((data, index) => (
+                    <ProjectCard key={index} data={data}/>
+                ))}
+            </div>
         </div>
     );
 }
